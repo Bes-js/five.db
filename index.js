5da8b15b9915388272f00b89cdc80a499664a88b
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-02-02 18:34:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-exports.bson = () => require("./src/drivers/bson");
-exports.sqlite = () => require("./src/drivers/sqlite");
-throw new Error("Bir Veritabanı Formatı Belirtmelisin [ Örn; 'const db = require('five.db').sqlite();' Veya 'const db = require('five.db').bson();' ]");
+exports.five = () => require("./src/drivers/bson");
